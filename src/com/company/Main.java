aad8647fbe2d25381eff13355e4cf46930e9eb95
vito <batkovito@abv.bg>
2020-02-24 20:30:36 +0200

    zero

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,9 +5,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int A = 0;
-        int b = 0;
+      
 
     }
 }
