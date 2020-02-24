aaa50011b8b3b8617df021175b844193ad715745
vito <batkovito@abv.bg>
2020-02-24 20:28:27 +0200

    two

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,13 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        
+        Scanner input = new Scanner(System.in);
+        int A = 0;
+        int b = 0;
 
     }
 }
